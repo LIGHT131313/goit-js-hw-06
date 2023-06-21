d06611e240d7e89cf8b10f2cd9407ebd62b196e5
LIGHT131313 <131313light@gmail.com>
2023-06-21 09:02:48 +0300

    fix 5 hw

--- js/task-05.js
+++ js/task-05.js
@@ -4,12 +4,6 @@ const outputName = document.querySelector('#name-output');
 inputName.addEventListener('input', onInputName);
 
 function onInputName(evt) {
-	if (!evt.currentTarget.value.trim() === false) {
-		outputName.textContent = evt.currentTarget.value.trim();
-	} else {
-		outputName.textContent = 'Anonymous';
-	}
-	// !evt.currentTarget.value.trim() === false
-	// 	? (outputName.textContent = evt.currentTarget.value.trim())
-	// 	: (outputName.textContent = 'Anonymous');
+	const trimmedValue = evt.currentTarget.value.trim();
+	outputName.textContent = trimmedValue !== '' ? trimmedValue : 'Anonymous';
 }
